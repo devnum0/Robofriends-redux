02760d8800240489e51ab10869ee752ce6e6da76
Kevinn89 <mitchell.kevinn@gmail>
2019-07-17 18:47:56 -0500

    first commit

---
+++ build/precache-manifest.d925c78957394b914968a938d780e91d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends-redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9ab3f333849154612149",
+    "url": "/Robofriends-redux/static/js/runtime~main.9ab3f333.js"
+  },
+  {
+    "revision": "aea3c0e33a9671658583",
+    "url": "/Robofriends-redux/static/js/main.aea3c0e3.chunk.js"
+  },
+  {
+    "revision": "c4c2a1785bad3432b592",
+    "url": "/Robofriends-redux/static/js/1.c4c2a178.chunk.js"
+  },
+  {
+    "revision": "aea3c0e33a9671658583",
+    "url": "/Robofriends-redux/static/css/main.163ae6fe.chunk.css"
+  },
+  {
+    "revision": "c4c2a1785bad3432b592",
+    "url": "/Robofriends-redux/static/css/1.8d03a239.chunk.css"
+  },
+  {
+    "revision": "a7e3dee8576690984816af27b5cc1870",
+    "url": "/Robofriends-redux/index.html"
+  }
+];
